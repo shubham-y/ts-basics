d3df92b82d433fd68c668ab340dc6d295add13b9
Shubham <yadav105shubham@gmail.com>
2022-06-23 18:32:23 +0530

    basics of functions

---
+++ functions-test.ts
@@ -0,0 +1,5 @@
+import addNumbers, { addStrings } from "./functions"
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+console.log(addStrings("a"));
---
+++ js-function-tests.js
@@ -0,0 +1,7 @@
+const { getName } = require("./functions");
+
+console.log(getName({ 
+    first: "Shubham",
+    last: "Yadav"
+}));
+console.log(getName());
